ad524dcb462c7ff9b34e1103cb70854fcc1b0c18
ahmed122222222222 <ahmedosamanasereldeeb@gmail.com>
2024-06-01 19:12:40 +0300

    finsh on bording

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:ecommerapp/core/helper_functions/on_generate_routes.dart';
+import 'package:ecommerapp/fetures/splash/presention/view/splash.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'generated/l10n.dart';
+
+void main() {
+  runApp(const FruitHub());
+}
+
+class FruitHub extends StatelessWidget {
+  const FruitHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: const Locale('ar'),
+      onGenerateRoute: onGenerateRoute,
+      initialRoute: SplashView.routeName,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
